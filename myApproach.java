983f552b3ec6d5b0be9fcea65e60890433a4b72c
deepthi248 <36158916+deepthi248@users.noreply.github.com>
2021-12-29 16:13:35 +0530

    Update myApproach.java

--- myApproach.java
+++ myApproach.java
@@ -2,28 +2,32 @@ import java.util.Arrays;
 public class interview1 {
     public static void main(String[] args) {
         int arr[] = {2, 1, 5, 3, 11, 7};
-        MyApproach(arr);
-        for(int i :arr){
-            System.out.println(i);
+        int [] result = MyApproach(arr);
+        for(int i :result){
+            System.out.print(i+" ");
         }
     }
 
     public static int[] MyApproach(int arr[]) {
         Arrays.sort(arr);
-        //tc 1&2
+
         if (arr.length == 1 || arr.length == 2)
             return arr;
 
-        //tc3
-        int result[] = new int[arr.length];
-        int l = 0, k = arr.length - 1;
-        for (int i = result.length - 2, j = 1; i >= 0 && j < result.length; i -= 2, j += 2) {
-            result[i] = arr[l];
-            l += 1;
-            result[j] = arr[k];
-            k--;
-        }
-        return result;
+        int length = arr.length - 1;
+        for (int i =1;i<length;i++) {
+                if(i%2 != 0){
+                    if(arr[i] <arr[i+1])
+                        swap(arr,i,i+1);
+                }
+            }
+        return arr;
+    }
+    
+    public static void swap(int [] arr, int i,int j){
+        int temp= arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 
 
